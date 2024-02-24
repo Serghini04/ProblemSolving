04e0b23ff6c1c588ca9ad919c83c6cf3e74282d1
root <root@Serghini>
2024-02-24 13:34:46 +0100

    ::done::

--- learn_cpp/l3/pro.cpp
+++ learn_cpp/l3/pro.cpp
@@ -39,62 +39,61 @@ int start()
     return nb;
 }
 
-void show_clients(t_client *data, int pos)
+void show_clients(const vector<t_client>& data)
 {
-	int i = 0;
-	char c;
-	system ("clear");
-	cout << setw(40) << "Client List (" << pos << ") Client(s).\n";
-	cout << "---------------------------------------------------------------------------\n";
-	cout << "| Accout Number | Pin Code | Client Name	  | Phone	 | Blance\n";
-	cout << "---------------------------------------------------------------------------\n";
-	while (i < pos)
-	{
-		cout << "|" <<  setw(15) << data[i].id << "|" <<  setw(10)  << data[i].pin << "|" <<  setw(22) << data[i].name << "|" <<  setw(14) << data[i].phone << "|" <<  setw(8) << data[i].balance << "\n";
-		i++;
-	}
-	cout << "Press any Key to go back to Main Menue ....\n";
-	cin >> c;
+    char c;
+    system("clear");
+    cout << setw(40) << "Client List (" << data.size() << ") Client(s).\n";
+    cout << "---------------------------------------------------------------------------\n";
+    cout << "| Account Number | Pin Code | Client Name       | Phone          | Balance\n";
+    cout << "---------------------------------------------------------------------------\n";
+    for (const t_client& info : data)
+    {
+        cout << "|" << setw(15) << info.id << "|" << setw(10) << info.pin << "|" << setw(22) << info.name << "|" << setw(14) << info.phone << "|" << setw(8) << info.balance << "\n";
+    }
+    cout << "Press any Key to go back to Main Menu ....\n";
+    cin >> c;
 }
 
-void	add_client(t_client *data, int *nbr_client)
+void	add_client(vector<t_client>& data)
 {
 	char c;
 	while (1)
 	{
+		t_client info;
 		system ("clear");
 		cout << "--------------------------------------------\n";
 		cout << "	Add New Clients Screen\n";
 		cout << "--------------------------------------------\n";
 		cout << "Adding New Client:\n";
 		cout << "Enter Account Number? ";
-		cin >> data[*nbr_client].id;
+		cin >> info.id;
 		cout << "Enter Pin Code? ";
-		cin >> data[*nbr_client].pin;
+		cin >> info.pin;
 		cout << "Enter Name? ";
-		cin >> data[*nbr_client].name;
+		cin >> info.name;
 		cout << "Enter Phone? ";
-		cin >> data[*nbr_client].phone;
+		cin >> info.phone;
 		cout << "Enter Accout balance? ";
-		cin >> data[*nbr_client].balance;
-		(*nbr_client)++;
+		cin >> info.balance;
 		cout << "Client Added Successfully, do you want to add more clients? (y/n)\n";
+		data.push_back(info);
 		cin >> c;
 		if (c != 'y')
 			break;
 	}
 }
-void info_client(t_client *data, int nb_client)
+void info_client(t_client& data)
 {
 	cout << "The following are the client delaits :\n";
-	cout << "Account Number" <<setw(4) << ":" << data[nb_client].id << "\n";
-	cout << "Pin Code" <<setw(7) << ":"  << data[nb_client].pin << "\n";
-	cout << "Name" <<setw(10) << ":"  << data[nb_client].name << "\n";
-	cout << "Phone" <<setw(9) << ":"  << data[nb_client].phone << "\n";
-	cout << "Account balance" <<setw(4) << ":"  << data[nb_client].id << "\n";
+	cout << "Account Number" <<setw(4) << ":" << data.id << "\n";
+	cout << "Pin Code" <<setw(7) << ":"  << data.pin << "\n";
+	cout << "Name" <<setw(10) << ":"  << data.name << "\n";
+	cout << "Phone" <<setw(9) << ":"  << data.phone << "\n";
+	cout << "Account balance" <<setw(4) << ":"  << data.id << "\n";
 	cout << "Are you sure you want update this client? (y/n)?\n";
 }
-void	delete_client(t_client *data, int *nb)
+void	delete_client(vector<t_client>& data)
 {
 	string c;
 	system("clear");
@@ -103,28 +102,21 @@ void	delete_client(t_client *data, int *nb)
 	cout << "----------------------------------\n";
 	cout << "Please enter Account Number\n";
 	cin >> c;
-	int i = 0;
 	int v = 0;
 	char input;
-	while (i < *nb)
+	for (auto it = data.begin(); it != data.end(); ++it)
 	{
-		if (data[i].id == c)
+		if (it->id == c)
 		{
-			info_client(data, i);
+			info_client(*it);
 			cout << "Are you sure you want delete this client? (y/n)\n";
 			cin >> input;
 			if (input == 'n')
 				break;
-			while (i < *nb - 1)
-			{
-				i++;
-				data[i] = data[i + 1];
-			}
-			(*nb)--;
+			data.erase(it);
 			v = 1;
 			break ;
 		}
-		i++;
 	}
 	if (v == 0)
 		cout << "Client with Account Number (" << c << ") is Not Found!\n";
@@ -133,7 +125,7 @@ void	delete_client(t_client *data, int *nb)
 	cin >> n;
 }
 
-void update_info(t_client *data, int nbr_client)
+void update_info(vector<t_client>& data)
 {
 	string input;
 	system("clear");
@@ -142,24 +134,24 @@ void update_info(t_client *data, int nbr_client)
 	cout << "----------------------------------\n";
 	cout << "Please enter Account Number?\n";
 	cin >> input;
-	for(int i = 0; i < nbr_client; i++)
+	for(auto it = data.begin(); it != data.end(); ++it)
 	{
-		if (input == data[i].id)
+		if (input == it->id)
 		{
-			info_client(data, i);
+			info_client(*it);
 			char c;
 			cout << "Are you sure you want update this client? (y/n)\n";
 			cin >> c;
 			if (c == 'n')
 				break;
 			cout << "Enter Pin Code? ";
-			cin >> data[i].pin;
+			cin >> it->pin;
 			cout << "Enter Name? ";
-			cin >> data[i].name;
+			cin >> it->name;
 			cout << "Enter Phone? ";
-			cin >> data[i].phone;
+			cin >> it->phone;
 			cout << "Enter Accout balance? ";
-			cin >> data[i].balance;	
+			cin >> it->balance;	
 			cout << "Client update successfully\n";
 			break;
 		}
@@ -168,7 +160,7 @@ void update_info(t_client *data, int nbr_client)
 	cin >> input;
 }
 
-void find_client(t_client *data, int nb_client)
+void find_client(vector<t_client>& data)
 {
 	int i = 0;
 	cout << "---------------------------------\n";
@@ -177,11 +169,11 @@ void find_client(t_client *data, int nb_client)
 	cout << "Please enter Account number?\n";
 	string input;
 	cin >> input;
-	while (i < nb_client)
+	for (auto it = data.begin(); it != data.end(); ++it)
 	{
-		if (input == data[i].id)
+		if (input == it->id)
 		{
-			info_client(data, i);
+			info_client(*it);
 			return ;
 		}
 	}
@@ -190,30 +182,101 @@ void find_client(t_client *data, int nb_client)
 	cin >> input;
 }
 
+vector<string> split(string s, string del)
+{
+    vector<string> res;
+    string word;
+    int pos;
+    while ((pos = s.find(del)) != std::string::npos)
+    {
+        word = s.substr(0, pos);
+        if (word != "")
+            res.push_back(word);
+        s.erase(0, pos + del.length());
+    }
+    if (s != "")
+        res.push_back(s);
+    return (res);
+}
+
+void read_form_file(vector<t_client>& data)
+{
+	fstream fd;
+	string line;
+	vector<string> info;
+	t_client fill;
+	fd.open("save_data.txt", ios::in | ios::out);
+	if (!fd.is_open()) {
+        cout << "Error opening file!" << endl;
+		exit(1);
+	}
+	if (fd.is_open())
+	{
+		while (getline(fd, line))
+		{
+			info = split(line, ">>>");
+			fill.id = info[0];
+			fill.name = info[1];
+			fill.pin = info[2];
+			fill.phone = info[3];
+			fill.balance = info[4];
+			data.push_back(fill);
+		}
+		fd.close();
+	}
+}
+string str_join(t_client& data)
+{
+	string line;
+	line = data.id + ">>>";
+	line = line + data.name + ">>>";
+	line = line + data.pin + ">>>";
+	line = line + data.phone + ">>>";
+	line = line + data.balance;
+	return (line);
+}
+
+void save_update(vector<t_client>& data)
+{
+	string line;
+	fstream fd;
+	fd.open("save_data.txt", ios::out);
+	if (!fd.is_open())
+	{
+		perror("you have error in open a file ...\n");
+		exit(1);
+	}
+	for (t_client& info : data)
+	{
+		line = str_join(info);
+		fd << line << line;
+	}
+	fd.close();
+}
+
 int main()
 {
 	char	nb;
-	t_client *data;
-	data = new t_client[2555];
-	int i = 0;
+	vector<t_client> data;
+	read_form_file(data);
 	int nbr_cline = 0;
     do
     {
         nb = start();
         if (nb == 1)
-            show_clients(data, nbr_cline);
+            show_clients(data);
         else if (nb == 2)
-            add_client(data, &nbr_cline);
+            add_client(data);
 		else if (nb == 3)
-			delete_client(data, &nbr_cline);
+			delete_client(data);
 		else if (nb == 4)
-			update_info(data, nbr_cline);
+			update_info(data);
 		else if (nb == 5)
-			find_client(data, nbr_cline);
-		i++;
+			find_client(data);
     } while (nb != 6);
 	system("clear");
+	save_update(data);
 	cout << "=================================\n";
-	cout << setw(40) << "end ...\n";
+	cout << setw(24) << ">>> end ...\n";
 	cout << "=================================\n";
 }
